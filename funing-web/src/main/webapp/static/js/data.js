9b73d830eaa5516831f0f66a1b1e57dffef77340
Dannis <danniszhd@163.com>
2015-05-29 01:23:14 +0800

    提交代码

---
+++ funing-web/src/main/webapp/static/js/data.js
@@ -0,0 +1,15 @@
+/**
+ * @author Dannis.
+ * @date 2015-05-29 00:25
+ */
+
+var operators = {
+    'ChinaMobile': '中国移动',
+    'ChinaUnicom': '中国联通',
+    'ChinaTelecom': '中国电信',
+    'VirtualNetworkOperator': '虚拟运营商'
+};
+
+var cities = [
+
+];
